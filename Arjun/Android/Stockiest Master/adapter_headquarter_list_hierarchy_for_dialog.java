ee26c1f1bf30c8944d0343a86f715a5642db7c10
ArjunaPatil <30894586+ArjunaPatil@users.noreply.github.com>
2017-08-24 15:14:16 +0530

    Create adapter_headquarter_list_hierarchy_for_dialog.java

---
+++ Arjun/Android/Stockiest Master/adapter_headquarter_list_hierarchy_for_dialog.java
@@ -0,0 +1,63 @@
+package com.example.vin.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by vin on 31/01/2017.
+ */
+
+
+public class adapter_headquarter_list_hierarchy_for_dialog extends ArrayAdapter<POJO_Territory_S> {
+    public Context _context;
+    public adapter_headquarter_list_hierarchy_for_dialog(Context context, int resource, List<POJO_Territory_S> POJO_Territory_S) {
+        super(context, resource, POJO_Territory_S);
+        this._context=context;
+        try {
+        } catch (Exception ex) {
+            //Toast.makeText(getContext(), ex.getMessage().toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+//((pp.getHeadquarter_id().equals(last_POJO.getHeadquarter_id()))) {
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+        try {
+
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.adapter_user_list_hirarchy_for_dialog, parent, false);
+        }
+
+        final POJO_Territory_S POJO_Territory_S = getItem(position);
+
+            //TextView test = (TextView) v.findViewById(R.id.test);
+
+        if (POJO_Territory_S != null) {
+            //test.setVisibility(View.VISIBLE);
+            TextView tvname = (TextView) v.findViewById(R.id.name);
+            //TextView head = (TextView) v.findViewById(R.id.head);
+            TextView sub = (TextView) v.findViewById(R.id.sub);
+
+            //head.setText(User.getFull_name());
+            sub.setText(POJO_Territory_S.getHeadquarter_name());
+
+            tvname.setText(POJO_Territory_S.getHeadquarter_id());
+        }
+
+        return v;
+        } catch (Exception ex) {
+            Toast.makeText(getContext(), ex.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            return v;
+        }
+    }
+}
